bc663155253a2ae1874ad2aa80773c29c62a364e
Bill <wtheisen@nd.edu>
2025-12-30 11:20:09 -0500

    feat: switch to socket server with bots

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 import { Server } from 'boardgame.io/server';
+import { RandomBot } from 'boardgame.io/ai';
 import { MossflowerGame } from './game/mossflower.js';
 
+const { PORT = 8000, CLIENT_ORIGIN = 'http://localhost:5173' } = process.env;
+
 const server = Server({
-  games: [MossflowerGame]
+  games: [
+    {
+      name: MossflowerGame.name,
+      game: MossflowerGame,
+      bots: {
+        '1': RandomBot,
+        '2': RandomBot,
+        '3': RandomBot
+      }
+    }
+  ],
+  origins: [CLIENT_ORIGIN, 'http://localhost:4173']
 });
 
-const PORT = Number(process.env.PORT) || 8000;
-
-server.run(PORT, () => {
+server.run(Number(PORT), () => {
   console.log(`Mossflower server running on http://localhost:${PORT}`);
+  console.log(`Allowing requests from ${CLIENT_ORIGIN}`);
 });
